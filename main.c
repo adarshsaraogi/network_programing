4bc4b124c1ad9d00beaefde020542d13072c9a41
adarsh <saraogiadarsh@gmail.com>
2018-11-17 20:41:35 +0530

    added addr_info_struct

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct addrinfo{
+  int ai_flags;
+  int ai_family;
+  int ai_socktype;
+  int ai_protocol;
+  size_t ai_addrlen;
+  struct sockaddr *ai_addr;
+  char *ai_canonname;
+  struct addrinfo *ai_next;
+};
+
+int main()
+{
+  printf("perfectly");
+  return 0;
+}
